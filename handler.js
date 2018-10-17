9164c4648669a147f9eed9c57675fc278fb19362
Iris Domínguez <irisontherise@gmail.com>
2018-10-17 18:12:47 +0200

    Totalorder first implementation

--- handler.js
+++ handler.js
@@ -8,6 +8,7 @@ var CONFIG = require('./constants.js');
 
 var routerLadoClients = zmq.socket('dealer');
 var socketLadoWorkers = zmq.socket('dealer');
+var socketTotalOrder = zmq.socket('dealer');
 
 if( process.argv.length < 3) {
 	console.log('Parametros incorrectos');
@@ -23,6 +24,9 @@ routerLadoClients.connect(CONFIG.IP_ROUTER1_HANDLER);
 socketLadoWorkers.identity = 'handler' + id;
 socketLadoWorkers.connect(CONFIG.IP_ROUTER2_HANDLER);
 
+socketTotalOrder.identity = 'handler' + id;
+socketTotalOrder.connect(CONFIG.IP_TOTALORDER);
+
 routerLadoClients.on('message', function(sender, packetRaw) {
 	var packetString = packetRaw.toString();
 	console.log('Handler received: ' + packetString);
@@ -36,6 +40,8 @@ routerLadoClients.on('message', function(sender, packetRaw) {
 		type: 'handler_request'
 	}
 	socketLadoWorkers.send(JSON.stringify(newPacket));
+	
+	socketTotalOrder.send(JSON.stringify(newPacket));
 });
 
 socketLadoWorkers.on('message', function(sender, packetRaw) {
@@ -45,4 +51,12 @@ socketLadoWorkers.on('message', function(sender, packetRaw) {
 	var newPacket = packet;
 	newPacket.target = packet.producer;
 	routerLadoClients.send(JSON.stringify(newPacket));
+});
+
+socketTotalOrder.on('message', function(sender, packetRaw) {
+	var packetString = packetRaw.toString();
+	var packet = JSON.parse(packetString);
+	console.log('Total order received: ' + packetString);
+	var order = packet.seq;
+	console.log('Total order for [' + packet.id + ']: ' + order);
 });
